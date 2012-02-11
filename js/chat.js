ab49a87e008c99ca8aa73f08b7295730084ce892
Evgeniy <evgeniyk@adyax.com>
2012-02-11 21:16:59 +0200

    Bad words filter implemented on client side for moderation: filtering is moved into ProcessReplaces

--- js/chat.js
+++ js/chat.js
@@ -233,7 +233,7 @@ function ReadChat( firstRead ){
 		$.getJSON( CHAT_URL + 'memfs/channel-moderator.json', function( jsonData ){
 			if ( jsonData != undefined ) {
 				var messageList = [];
-				messageList = FilterMessages(jsonData.messages);
+				messageList = jsonData.messages;
 				data = BuildHtml( messageList );
 				PutDataToChat( data );
 			}
@@ -256,9 +256,7 @@ function ReadChat( firstRead ){
 function FilterMessages(messages) {
     var pattern = /\w{0,5}[хx]([хx\s\!@#\$%\^&*+-\|\/]{0,6})[уy]([уy\s\!@#\$%\^&*+-\|\/]{0,6})[ёiлeеюийя]\w{0,7}|\w{0,6}[пp]([пp\s\!@#\$%\^&*+-\|\/]{0,6})[iие]([iие\s\!@#\$%\^&*+-\|\/]{0,6})[3зс]([3зс\s\!@#\$%\^&*+-\|\/]{0,6})[дd]\w{0,10}|[сcs][уy]([уy\!@#\$%\^&*+-\|\/]{0,6})[4чkк]\w{1,3}|\w{0,4}[bб]([bб\s\!@#\$%\^&*+-\|\/]{0,6})[lл]([lл\s\!@#\$%\^&*+-\|\/]{0,6})[yя]\w{0,10}|\w{0,8}[её][bб][лске@eыиаa][наи@йвл]\w{0,8}|\w{0,4}[еe]([еe\s\!@#\$%\^&*+-\|\/]{0,6})[бb]([бb\s\!@#\$%\^&*+-\|\/]{0,6})[uу]([uу\s\!@#\$%\^&*+-\|\/]{0,6})[н4ч]\w{0,4}|\w{0,4}[еeё]([еeё\s\!@#\$%\^&*+-\|\/]{0,6})[бb]([бb\s\!@#\$%\^&*+-\|\/]{0,6})[нn]([нn\s\!@#\$%\^&*+-\|\/]{0,6})[уy]\w{0,4}|\w{0,4}[еe]([еe\s\!@#\$%\^&*+-\|\/]{0,6})[бb]([бb\s\!@#\$%\^&*+-\|\/]{0,6})[оoаa@]([оoаa@\s\!@#\$%\^&*+-\|\/]{0,6})[тnнt]\w{0,4}|\w{0,10}[ё]([ё\!@#\$%\^&*+-\|\/]{0,6})[б]\w{0,6}|\w{0,4}[pп]([pп\s\!@#\$%\^&*+-\|\/]{0,6})[иeеi]([иeеi\s\!@#\$%\^&*+-\|\/]{0,6})[дd]([дd\s\!@#\$%\^&*+-\|\/]{0,6})[oоаa@еeиi]([oоаa@еeиi\s\!@#\$%\^&*+-\|\/]{0,6})[рr]\w{0,12}|\w{0,6}[сsc]([сsc\s\!@#\$%\^&*+-\|\/]{0,6})[уyu]([уyu\s\!@#\$%\^&*+-\|\/]{0,6})[кk]\w{0,10}/gi;
 
-    for (i=0; i < messages.length; i++) {
-        messages[i].message = messages[i].message.replace(pattern, '<b><font color="red">[$&]</font></b>');
-    }
+    messages = messages.replace(pattern, '<b><font color="red">[$&]</font></b>');
 
     return messages;
 }
@@ -318,7 +316,7 @@ function ProcessReplaces( str ) {
 		, 'gi'
 	);
 	str = str.replace( urlPattern, MakeShrinkUrl );
-	
+    str = FilterMessages(str);
 	return str;
 }
 
