a2a998c832b24eadc10b898ac9c16ba43f0f2d81
Evgeniy <evgeniyk@adyax.com>
2012-03-05 15:21:05 +0200

    Life streamer role added

--- chat.php
+++ chat.php
@@ -431,7 +431,7 @@ class Chat {
     			date > "' . date( 'Y-m-d H:i:s', CURRENT_TIME - 259200 ) . '" AND
           deletedBy is NULL
           ORDER BY id DESC LIMIT '. $messagesCount * 3 . '
-        ) as tmp_table_chat  ORDER BY FIELD(rid,3,4,5,9,6,7,10,13,2) ASC LIMIT '. $messagesCount * 3 . '
+        ) as tmp_table_chat  ORDER BY FIELD(rid,3,4,5,9,6,7,10,14,2) ASC LIMIT '. $messagesCount * 3 . '
       ) as tmp_table_chat_limited GROUP BY id ORDER BY id DESC LIMIT '. $messagesCount;
 		
 		$queryResult = $this->db->Query( $queryString );
