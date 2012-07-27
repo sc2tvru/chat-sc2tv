cdcc386f07244c58b25b5a231e032426d9bc6f06
chat.sc2tv.ru <chat@sc2tv.ru>
2012-07-27 22:57:02 +0400

    Fix regexp for smile-off button. shr

--- js/chat.js
+++ js/chat.js
@@ -770,7 +770,7 @@ function BuildHtml( messageList ) {
 		if( $.cookie( 'chat-img' ) == '0' ) {
 			msg = messageList[ i ].message;
 			msg = msg.replace( /[\s]+/g, '' );
-			regexp = /^(<b>[^>]*<\/b>[,\s]*){0,}[\s]*(:s:[^:]+:[\s]*){1,}[\s]*$/gi;
+			regexp = /^(<b>[^<]*<\/b>[,\s]*){0,}[\s]*(:s:[^:]+:[\s]*){1,}[\s]*$/gi;
 			smileOnly = regexp.test( msg );
 		}
 		
