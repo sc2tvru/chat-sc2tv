7b08d8aea381caadcb076bad9ad3c83b1ce68a60
shr <forshr@gmail.com>
2013-04-01 01:28:08 +0400

    Some magic for proper moderator stats reset every month.

--- cron_clear_moderators_details.php
+++ cron_clear_moderators_details.php
@@ -3,5 +3,6 @@ require_once 'core.php';
 require_once 'utils.php';
 global $memcache;
 $memcache->Delete( MODERATORS_DETAILS_MEMCACHE_KEY );
+unlink( CHAT_MODERATORS_DETAILS );
 SaveForDebug( 'cron clear moderators details' );
 ?>
