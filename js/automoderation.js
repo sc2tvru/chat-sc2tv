52fcc3baf6d5e2b7fa878970ada882fc88fefe6e
shr <forshr@gmail.com>
2013-11-04 21:37:14 +0400

    Try to fix url regexp. Fix link color in history.

--- js/automoderation.js
+++ js/automoderation.js
@@ -24,8 +24,8 @@ var urlPattern = '((?:(?:ftp)|(?:https?))(?:://))' + // протокол (1)
 	'|(?:(?:\\d{1,3}\\.){3}\\d{1,3}))' + // хост в формате IPv4 (3)
 	'(:\\d+)?' + // порт (4)
 	'(/[-a-z\u0430-\u0451\\d%_~\\+\\(\\):]*(?:[\\.,][-a-z\u0430-\u0451\\d%_~\\+\\(\\):]+)*)*' + // путь (5)
-	'(\\?(?:&amp;|[:;a-z\u0430-\u0451\\d%_~\\+=-])*)?' + // параметры (6)
-	'(#(?:&amp;|[:;a-z\u0430-\u0451\\d%_~\\+=-])*)?)'; // якорь (7)
+	'(\\?(?:&amp;|&quot;|&#039|[&"\'.:;a-z\u0430-\u0451\\d%_~\\+=-])*)?' + // параметры (6)
+	'(#(?:&amp;|&quot;|&#039|[&"\'.:;a-z\u0430-\u0451\\d%_~\\+=-])*)?)'; // якорь (7)
 	
 var bbToUrlPattern = new RegExp('\\[url\\]' + urlPattern + '\\[\/url\\]()', 'gi');//пусто(8)
 var bbToUrlPatternWithText = new RegExp('\\[url=' + urlPattern + '\\]([\u0020-\u007E\u0400-\u045F\u0490\u0491\u0207\u0239\u2012\u2013\u2014]+?)\\[\/url\\]', 'gi');//текст для ссылки(8)
@@ -256,14 +256,16 @@ function BuildHtml( messageList ) {
 
 //преобразуем бб код в хтмл
 function bbCodeUrlToHtml(str, proto, url, host, port, path, query, fragment, text){
-		url = url.replace(/:s:/g, '');	//удаляем смайлы из ссылок
-		if (!text) text = url;
-	    if ( text.length > 60 ) {
-			length = text.length;
-			return '<a rel="nofollow" href="' + proto + url + '" target="_blank" title="' + proto + url + '">' + text.substring( 0, 30 ) + '...' + text.substring( length - 20) + '</a>';
-		}
-		return '<a rel="nofollow" href="' + proto + url + '" title="' + text + '" target="_blank">' + text + '</a>';
-
+	//удаляем смайлы из ссылок
+	url = url.replace(/:s:/gi, ':%73:' );
+	if (!text) {
+		text = url;
+	}
+	if ( text.length > 60 ) {
+		length = text.length;
+		return '<a rel="nofollow" href="' + proto + url + '" target="_blank" title="' + proto + url + '">' + text.substring( 0, 30 ) + '...' + text.substring( length - 20) + '</a>';
+	}
+	return '<a rel="nofollow" href="' + proto + url + '" title="' + proto + url + '" target="_blank">' + text + '</a>';
 }
 
 // всевозможные замены
