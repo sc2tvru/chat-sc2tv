bea52099dc7e9418c60b5c1feb63c6bbe0e34d3e
KingCobra <kingcobra@yandex.ru>
2015-05-22 16:09:19 +0300

    fs_sync_last_messages

--- fs_sync_last_messages.php
+++ fs_sync_last_messages.php
@@ -1,6 +1,8 @@
 <?php
-require_once 'chat_config.php';
-require_once 'db.php';
+require_once 'core.php';
+if(!isset($_POST['key']) || $_POST['key'] !== FS_TO_CHAT_KEY){
+	exit;
+}
 date_default_timezone_set('Europe/Moscow');
 $db = new MySqlDb( CHAT_DB_HOST, CHAT_DB_NAME, CHAT_DB_USER, CHAT_DB_PASSWORD );
 $queryString = 'SELECT * FROM `chat_message` WHERE `date` > "' . date('Y-m-d H:i:s', time() - 300) . '" ORDER BY `id` DESC';
@@ -18,7 +20,5 @@ while ( $msg = $queryResult->fetch_assoc() ) {
 		'date' => $msg['date']
 	);
 }
-$dataJson = json_encode( $messages );
-$channelsFile = CHAT_MEMFS_DIR . '/fs_sync.json';
-file_put_contents( $channelsFile, $dataJson );
+print json_encode( $messages );
 ?>
