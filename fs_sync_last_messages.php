443488a3170d618d75c27d56a42664954f2deb61
KingCobra <kingcobra@yandex.ru>
2015-05-22 16:37:49 +0300

    fs_sync_last_messages with memcache

--- fs_sync_last_messages.php
+++ fs_sync_last_messages.php
@@ -1,14 +1,27 @@
 <?php
+date_default_timezone_set('Europe/Moscow');
 require_once 'core.php';
 if(!isset($_POST['key']) || $_POST['key'] !== FS_TO_CHAT_KEY){
 	exit;
 }
-date_default_timezone_set('Europe/Moscow');
+global $memcache;
+$memcache_key = 'fs_sync_last_time';
+$last_time = $memcache->Get( $memcache_key );	
+if ( $last_time === FALSE ) {
+	$last_time = CURRENT_TIME - 300;
+}
 $db = new MySqlDb( CHAT_DB_HOST, CHAT_DB_NAME, CHAT_DB_USER, CHAT_DB_PASSWORD );
-$queryString = 'SELECT * FROM `chat_message` WHERE `date` > "' . date('Y-m-d H:i:s', time() - 300) . '" ORDER BY `id` DESC';
+$queryString = 'SELECT * FROM `chat_message` WHERE `date` > "' . date('Y-m-d H:i:s', $last_time) . '" ORDER BY `id` DESC';
 $queryResult = $db->Query( $queryString );
 if ( $queryResult === FALSE ) {
 	exit;
+}
+if($memcache->memcache->replace($memcache_key, CURRENT_TIME, false, 300) === FALSE){
+	$memcache->Set(
+		$memcache_key,
+		CURRENT_TIME,
+		300
+	);
 }
 $messages = array();
 while ( $msg = $queryResult->fetch_assoc() ) {
